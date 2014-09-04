7c5e94109e7eda6ab3ffca9b3059d9398fc2d514
Sbreighs <stefano.bragaglia@gmail.com>
2014-09-04 11:16:10 +0100

    More or less stable, still room for enhancements, need to fix the classloader

--- src/main/java/bragaglia/skimmer/core/MemoryFileManager.java
+++ src/main/java/bragaglia/skimmer/core/MemoryFileManager.java
@@ -20,9 +20,26 @@ import javax.tools.StandardJavaFileManager;
  */
 public class MemoryFileManager extends ForwardingJavaFileManager<StandardJavaFileManager> {
 
-	// private MemoryJavaClassObject object;
-
 	private Map<String, MemoryJavaClassObject> objects;
+
+	private SecureClassLoader loader = new SecureClassLoader() {
+
+		private Map<String, Class<?>> classes = new HashMap<>();
+
+		@Override
+		protected Class<?> findClass(String name) throws ClassNotFoundException {
+			Class<?> result = classes.get(name);
+			if (null == result) {
+				MemoryJavaClassObject object = objects.get(name);
+				if (null == object)
+					throw new ClassNotFoundException("Class '" + name + "' not found in this ClassLoader.");
+				byte[] b = object.getBytes();
+				classes.put(name, super.defineClass(name, b, 0, b.length));
+				objects.remove(name);
+			}
+			return result;
+		}
+	};
 
 	public MemoryFileManager(StandardJavaFileManager manager) {
 		super(manager);
@@ -31,16 +48,7 @@ public class MemoryFileManager extends ForwardingJavaFileManager<StandardJavaFil
 
 	@Override
 	public ClassLoader getClassLoader(Location location) {
-		return new SecureClassLoader() {
-			@Override
-			protected Class<?> findClass(String name) throws ClassNotFoundException {
-				MemoryJavaClassObject object = objects.get(name);
-				if (null == object)
-					throw new ClassNotFoundException("Class '" + name + "' not found.");
-				byte[] b = object.getBytes();
-				return super.defineClass(name, b, 0, b.length);
-			}
-		};
+		return loader;
 	}
 
 	@Override
